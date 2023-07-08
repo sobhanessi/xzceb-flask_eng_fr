4a68f6d45f9716341fcae4f04c4f9d6390a7952c
sobhanessi <sobhan@esfandyari.com>
2023-07-08 07:35:55 -0400

    nothing

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,15 +1,13 @@
+''' writing functionality for translating english to french and vise versa'''
 from deep_translator import MyMemoryTranslator
 
-def englishToFrench(englishText):
+def english_to_french(english_text):
+    ''' takes en text and translates it to french!'''
+    french_text = MyMemoryTranslator(source="en-US", target="fr-FR").translate(english_text)
+    return french_text
 
-    frenchText = MyMemoryTranslator(source="en-US", target="fr-FR").translate(englishText)
-
-    return frenchText
-
-
-def frenchToEnglish(frenchText):
-
-    englishText = MyMemoryTranslator(source="fr-FR", target="en-US").translate(frenchText)
-
-    return englishText
+def french_to_english(french_text):
+    ''' takes fr text and translates it to english!'''
+    english_text = MyMemoryTranslator(source="fr-FR", target="en-US").translate(french_text)
+    return english_text
 
